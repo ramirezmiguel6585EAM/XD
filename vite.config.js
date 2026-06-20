9736e8f2d0397ae0425df02afdac4baf5b274c3f
ramirezmiguel6585EAM <ramirez.miguel.6585@eam.edu.co>
2026-06-20 15:19:41 -0500

    Añade Nuevo Omnimarket

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+process.env.BROWSER = 'C:\\Program Files\\Opera GX\\Launcher.exe';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Omnimarket/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: { open: true },
 })
-
